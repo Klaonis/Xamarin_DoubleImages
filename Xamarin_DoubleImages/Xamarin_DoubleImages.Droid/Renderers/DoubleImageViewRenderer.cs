03ba9c3d4ab4abe7b83f60fb617e3429b4e08667
Nicoco <klaonis.sazuqef@gmail.com>
2016-12-03 18:15:36 +0100

    First commit
    Project creation avec Form & android part

---
+++ Xamarin_DoubleImages/Xamarin_DoubleImages.Droid/Renderers/DoubleImageViewRenderer.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin_DoubleImages.Views;
+using Xamarin_DoubleImages.Droid.Renderers;
+using Xamarin.Forms.Platform.Android;
+using System.Windows.Input;
+
+[assembly: ExportRenderer(typeof(DoubleImageView), typeof(DoubleImageViewRenderer))]
+namespace Xamarin_DoubleImages.Droid.Renderers
+{
+    class DoubleImageViewRenderer : ViewRenderer
+    {
+        private ICommand _updateUICommand;
+
+        protected override void Dispose(bool disposing)
+        {
+            //MANAGE touch events
+            if (_updateUICommand != null)
+            {
+                Control.Touch -= Image_Touch;
+            }
+
+            _updateUICommand = null;
+
+            base.Dispose(disposing);
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.View> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                var view = (Context as Activity).LayoutInflater.Inflate(Resource.Layout.DoubleImageLayout, this, false);
+                var firstImage = view.FindViewById<ImageView>(Resource.Id.first_imageview);
+                var secondImage = view.FindViewById<ImageView>(Resource.Id.second_imageview);
+                // ??????
+                SetNativeControl(view);
+            }
+
+            if (Control != null)
+            {
+                DoubleImageView formView = (e.NewElement as DoubleImageView);
+
+                ExtractFormData(formView);
+
+                //MANAGE touch events
+                if (_updateUICommand != null)
+                {
+                    Control.Touch += Image_Touch;
+                }
+            }
+        }
+
+        private void Image_Touch(object sender, TouchEventArgs e)
+        {
+            var handled = false;
+            if (e.Event.Action == MotionEventActions.Down)
+            {
+                _updateUICommand.Execute(true);
+                handled = true;
+            }
+            else if (e.Event.Action == MotionEventActions.Up)
+            {
+                _updateUICommand.Execute(false);
+
+                handled = true;
+            }
+
+            e.Handled = handled;
+        }
+
+        /// <summary>
+        /// Extract form view data
+        /// </summary>
+        /// <param name="formImage">form view object</param>
+        private void ExtractFormData(DoubleImageView formView)
+        {
+            _updateUICommand = formView.SwitchImageCommand;
+        }
+    }
+}
---
+++ Xamarin_DoubleImages/Xamarin_DoubleImages/Views/DoubleImageView.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Xamarin_DoubleImages.Views
+{
+    public partial class DoubleImageView : ContentView
+    {
+        public static readonly BindableProperty FirstImageSourceProperty = BindableProperty.Create("FirstImageSource", typeof(string), typeof(DoubleImageView), string.Empty);
+        public string FirstImageSource
+        {
+            set { SetValue(FirstImageSourceProperty, value); }
+            get { return (string)GetValue(FirstImageSourceProperty); }
+        }
+
+
+        public static readonly BindableProperty ImageSizeProperty = BindableProperty.Create("ImageSize", typeof(int), typeof(DoubleImageView), 44);
+        public int ImageSize
+        {
+            set { SetValue(ImageSizeProperty, value); }
+            get { return (int)GetValue(ImageSizeProperty); }
+        }
+
+
+        public static readonly BindableProperty SecondImageSourceProperty = BindableProperty.Create("SecondImageSource", typeof(string), typeof(DoubleImageView), string.Empty);
+        public string SecondImageSource
+        {
+            set { SetValue(SecondImageSourceProperty, value); }
+            get { return (string)GetValue(SecondImageSourceProperty); }
+        }
+
+
+        public ICommand SwitchImageCommand { get; set; }
+
+        public DoubleImageView()
+        {
+            BindingContext = this;
+            SwitchImageCommand = new Command<bool>((bool isPressed) => UpdateImages(isPressed));
+            InitializeComponent();
+        }
+
+        private void UpdateImages(bool isPressed)
+        {
+            string source;
+            if (isPressed)
+            {
+                source = SecondImageSource;
+                SecondImageSource = FirstImageSource;
+                FirstImageSource = source;
+            }
+            else
+            {
+                source = FirstImageSource;
+                FirstImageSource = SecondImageSource;
+                SecondImageSource = source;
+            }
+        }
+    }
+}
